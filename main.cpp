2961885d23e561f24d5c2fcc73ddd9d9f4e35cdd
Kristopher Horton <khorton@miracosta.edu>
2025-07-17 09:27:59 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,31 @@
 #include <vector>
 #include <iostream>
+// #include "OpenSourceHardware.h"
+// #include "Arduino.h"
+// #include "RaspberryPi.h"
 
 int main() {
-    // Arduino nano = new Arduino("Nano", 5, 21.16, "ATmega328");
-    // Arduino uno3 = new Arduino("UNO Rev 3", 5, 23.46, "ATmega328P");
-    // RaspberryPi model5 = new RaspberryPi("5 Model 8GB", 5, 80.00, "Raspbian");
-    // RaspberryPi model4 = new RaspberryPi("4 Model B", 5, 45.00, "Raspbian");
+    // Arduino* nano = new Arduino("Nano", 5, 21.16, "ATmega328");
+    // Arduino* uno3 = new Arduino("UNO Rev 3", 5, 23.46, "ATmega328P");
+    // RaspberryPi* model5 = new RaspberryPi("5 Model 8GB", 5, 80.00, "Raspbian");
+    // RaspberryPi* model4 = new RaspberryPi("4 Model B", 5, 45.00, "Raspbian");
 
-    // vector<OpenSourceHardware> inventory;
+    //vector<OpenSourceHardware> inventory;
 
-    // inventory.push_back(nano);
-    // inventory.push_back(uno3);
-    // inventory.push_back(model5);
-    // inventory.push_back(model4);
+    // inventory.push_back(*nano);
+    // inventory.push_back(*uno3);
+    // inventory.push_back(*model5);
+    // inventory.push_back(*model4);
 
-    // cout << inventory << endl;
+    // for (OpenSourceHardware osh : inventory) {
+    //     cout << osh << "\n";
+    // }
+
+    // for (int i = 0; i < inventory.size(); i++) {
+    //     // recover memory!
+    //     delete inventory[i];
+    //     inventory[i] = NULL;
+    // }
 
     return 0;
 }
